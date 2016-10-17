0e99e0ba9c9f45159c83aa607c16f1d065790d29
kyorohiro <kyorohiro@gmail.com>
2016-10-18 00:23:13 +0900

    div file

---
+++ finder.go
@@ -0,0 +1,82 @@
+package miniblob
+
+import (
+	"golang.org/x/net/context"
+
+	"google.golang.org/appengine/datastore"
+)
+
+/*
+- kind: BlobItem
+  properties:
+  - name: Parent
+  - name: Updated
+    direction: asc
+
+- kind: BlobItem
+  properties:
+  - name: Parent
+  - name: Updated
+    direction: desc
+https://cloud.google.com/appengine/docs/go/config/indexconfig#updating_indexes
+*/
+func (obj *BlobManager) FindBlobItemFromParent(ctx context.Context, parent string, cursorSrc string) ([]*BlobItem, string, string) {
+	//
+	q := datastore.NewQuery(obj.blobItemKind)
+	q = q.Filter("ProjectId =", obj.projectId)
+	q = q.Filter("Parent =", parent)
+	q = q.Order("-Updated")
+	//
+	return obj.FindBlobItemFromQuery(ctx, q, cursorSrc)
+}
+
+//
+//
+func (obj *BlobManager) FindBlobItemFromQuery(ctx context.Context, q *datastore.Query, cursorSrc string) ([]*BlobItem, string, string) {
+	cursor := obj.newCursorFromSrc(cursorSrc)
+	if cursor != nil {
+		q = q.Start(*cursor)
+	}
+	q = q.KeysOnly()
+	founds := q.Run(ctx)
+
+	var retUser []*BlobItem
+
+	var cursorNext string = ""
+	var cursorOne string = ""
+
+	for i := 0; ; i++ {
+		key, err := founds.Next(nil)
+		if err != nil || err == datastore.Done {
+			break
+		} else {
+			v, e := obj.NewBlobItemFromGaeObjectKey(ctx, key)
+			if e == nil {
+				retUser = append(retUser, v)
+			}
+		}
+		if i == 0 {
+			cursorOne = obj.makeCursorSrc(founds)
+		}
+	}
+	cursorNext = obj.makeCursorSrc(founds)
+	return retUser, cursorOne, cursorNext
+}
+
+func (obj *BlobManager) newCursorFromSrc(cursorSrc string) *datastore.Cursor {
+	c1, e := datastore.DecodeCursor(cursorSrc)
+	if e != nil {
+		return nil
+	} else {
+		return &c1
+	}
+}
+
+func (obj *BlobManager) makeCursorSrc(founds *datastore.Iterator) string {
+	c, e := founds.Cursor()
+	if e == nil {
+		return c.String()
+	} else {
+		return ""
+	}
+}
--- manager.go
+++ manager.go
@@ -52,14 +52,7 @@ func NewBlobManager(projectId string, uploadUrlBase string, blobItemKind string)
 
 func (obj *BlobManager) GetBlobItem(ctx context.Context, parent string, name string, isNew bool) (*BlobItem, error) {
 	key := obj.NewBlobItemKey(ctx, parent, name)
-	ret, err := obj.NewBlobItemFromGaeObjectKey(ctx, key)
-
-	if err == nil {
-		ret.updateMemcache(ctx)
-		return ret, nil
-	} else {
-		return nil, err
-	}
+	return obj.NewBlobItemFromGaeObjectKey(ctx, key)
 }
 
 func (obj *BlobManager) MakeStringId(parent string, name string) string {
@@ -98,80 +91,9 @@ func (obj *BlobManager) NewBlobItemFromGaeObjectKey(ctx context.Context, gaeKey 
 	ret := new(BlobItem)
 	ret.gaeObject = &item
 	ret.gaeObjectKey = gaeKey
+
+	if err == nil {
+		ret.updateMemcache(ctx)
+	}
 	return ret, nil
-}
-
-/*
-- kind: BlobItem
-  properties:
-  - name: Parent
-  - name: Updated
-    direction: asc
-
-- kind: BlobItem
-  properties:
-  - name: Parent
-  - name: Updated
-    direction: desc
-https://cloud.google.com/appengine/docs/go/config/indexconfig#updating_indexes
-*/
-func (obj *BlobManager) FindBlobItemFromParent(ctx context.Context, parent string, cursorSrc string) ([]*BlobItem, string, string) {
-	//
-	q := datastore.NewQuery(obj.blobItemKind)
-	q = q.Filter("ProjectId =", obj.projectId)
-	q = q.Filter("Parent =", parent)
-	q = q.Order("-Updated")
-	//
-	return obj.FindBlobItemFromQuery(ctx, q, cursorSrc)
-}
-
-//
-//
-func (obj *BlobManager) FindBlobItemFromQuery(ctx context.Context, q *datastore.Query, cursorSrc string) ([]*BlobItem, string, string) {
-	cursor := obj.newCursorFromSrc(cursorSrc)
-	if cursor != nil {
-		q = q.Start(*cursor)
-	}
-	q = q.KeysOnly()
-	founds := q.Run(ctx)
-
-	var retUser []*BlobItem
-
-	var cursorNext string = ""
-	var cursorOne string = ""
-
-	for i := 0; ; i++ {
-		key, err := founds.Next(nil)
-		if err != nil || err == datastore.Done {
-			break
-		} else {
-			v, e := obj.NewBlobItemFromGaeObjectKey(ctx, key)
-			if e == nil {
-				retUser = append(retUser, v)
-			}
-		}
-		if i == 0 {
-			cursorOne = obj.makeCursorSrc(founds)
-		}
-	}
-	cursorNext = obj.makeCursorSrc(founds)
-	return retUser, cursorOne, cursorNext
-}
-
-func (obj *BlobManager) newCursorFromSrc(cursorSrc string) *datastore.Cursor {
-	c1, e := datastore.DecodeCursor(cursorSrc)
-	if e != nil {
-		return nil
-	} else {
-		return &c1
-	}
-}
-
-func (obj *BlobManager) makeCursorSrc(founds *datastore.Iterator) string {
-	c, e := founds.Cursor()
-	if e == nil {
-		return c.String()
-	} else {
-		return ""
-	}
 }
